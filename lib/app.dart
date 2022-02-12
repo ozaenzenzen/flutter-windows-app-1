5b3ac4382851903c7682f4c14f47f169fa2684e6
Fauzan Akmal Mahdi <recovery252@gmail.com>
2022-02-13 06:21:44 +0700

    update app

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_windows_app_1/homepage.dart';
+import 'package:flutter_windows_app_1/homepage_fluentstyle.dart';
 import 'package:get/get.dart';
 
 class MyApp extends StatefulWidget {
@@ -14,15 +16,17 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 640),
+      // designSize: const Size(360, 640), // mobile
+      designSize: const Size(1080, 1920), // desktop
       builder: () {
-        return GetMaterialApp(
+        return FluentApp(
           home: Builder(
             builder: (context) {
               ScreenUtil.setContext(context);
               return const MediaQuery(
                 data: MediaQueryData(textScaleFactor: 1.0),
-                child: HomePage(),
+                // child: HomePage(),
+                child: HomePageFluentStyle(),
               );
             },
           ),
