1d757b961e8d228e18fc632a4c9bba0673067a2a
Fauzan Akmal Mahdi <recovery252@gmail.com>
2022-02-15 06:31:53 +0700

    update homepage fluent

--- lib/homepage_fluentstyle.dart
+++ lib/homepage_fluentstyle.dart
@@ -1,6 +1,7 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/material.dart' as material;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_windows_app_1/secondpage_fluentstyle.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePageFluentStyle extends StatefulWidget {
@@ -15,27 +16,65 @@ class _HomePageFluentStyleState extends State<HomePageFluentStyle> {
   Widget build(BuildContext context) {
     return ScaffoldPage(
       header: Container(
+        alignment: Alignment.center,
         color: Colors.teal,
-        height: 30.h,
+        height: 50,
+        child: Text(
+          "This is Header",
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+          ),
+        ),
       ),
       content: Container(
         alignment: Alignment.center,
         child: ListView.builder(
-          itemCount: 20,
-          itemBuilder: (context, index) {
-            return material.SelectableText(
-              "$index. Hai",
-              style: GoogleFonts.poppins(
-                fontSize: 40.sp,
-                fontWeight: FontWeight.w700
-              ),
-            );
-          }
-        ),
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return material.SelectableText(
+                "$index. Hai",
+                style: GoogleFonts.poppins(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w700,
+                ),
+              );
+            }),
       ),
       bottomBar: Container(
         color: Colors.red,
-        height: 30,
+        height: 50,
+        alignment: Alignment.center,
+        child: TextButton(
+          onPressed: () {
+            // Get.to();
+            Navigator.push(
+              context,
+              material.MaterialPageRoute(
+                builder: (context) => const SecondPageFluentStyle(),
+              ),
+            );
+          },
+          child: Text(
+            "Go To Another Page",
+            style: GoogleFonts.poppins(
+              // fontSize: 15.sp,
+              fontSize: 15,
+              // fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+          ),
+          style: ButtonStyle(
+            // backgroundColor: ButtonState.all<Color>(Colors.white),
+            foregroundColor: ButtonState.all<Color>(Colors.white),
+            // textStyle: ButtonState.all<TextStyle>(
+            //   GoogleFonts.poppins(
+            //     fontSize: 15,
+            //     // fontWeight: FontWeight.w700,
+            //     color: Colors.white,
+            //   ),
+            // ),
+          ),
+        ),
       ),
     );
   }
